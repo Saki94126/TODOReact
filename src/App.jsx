deda40d39c3639b5d7243cc9dc4b5d093959c86f
Saki94126 <104111843+Saki94126@users.noreply.github.com>
2022-05-12 19:11:36 +0900

    追加部分のコンポーネント化

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import React, { useState } from "react";
 import "./style.css";
+import { InputTodo } from "./components/InputTodo";
 
 export const App = () => {
-  const [todotext, setTodoText] = useState("");
+  const [todoText, setTodoText] = useState("");
   const [incompleteTodos, setIncompleteTodos] = useState([]);
   const [completeTodos, setCompleteTodos] = useState([]);
 
   const onChangeTodoText = (event) => setTodoText(event.target.value);
 
   const onClickAdd = () => {
-    if (todotext === "") return;
-    const newTodos = [...incompleteTodos, todotext];
+    if (todoText === "") return;
+    const newTodos = [...incompleteTodos, todoText];
     setIncompleteTodos(newTodos);
     setTodoText("");
   };
@@ -41,14 +42,11 @@ export const App = () => {
 
   return (
     <>
-      <div className="input-area">
-        <input
-          placeholder="TODOを入力"
-          value={todotext}
-          onChange={onChangeTodoText}
-        />
-        <button onClick={onClickAdd}>追加</button>
-      </div>
+      <InputTodo
+        todoText={todoText}
+        onChange={onChangeTodoText}
+        onClick={onClickAdd}
+      />
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
         <ul>
